6ab16add78a7179a23bc1cfbe678c31d5d0d8e74
Arjun Karki <arzoonkarki98@gmail.com>
2026-09-09 10:53:32 +0000

    add eligibletovote.java

---
+++ eligibletovote.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class eligibletovote
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int age;
+
+        System.out.print("Enter your age: ");
+        age = sc.nextInt();
+
+        if (age >= 18)
+        {
+            System.out.println("Eligible to vote");
+        }
+        else
+        {
+            System.out.println("Not eligible to vote");
+        }
+        sc.close();
+    }
+}
